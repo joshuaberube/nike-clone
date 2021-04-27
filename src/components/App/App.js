7cd4a75267407b9e95d21cee04b980c0efd50b34
Josh Berube <joshaberube@gmail.com>
2021-04-27 12:03:37 -0600

    removed unnecessary import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import Header from '../Header/Header'
-import Topbar from '../Header/Topbar/Topbar'
 import './App.scss'
 
 function App() {
   return (
     <div>
       <Header />
-      
     </div>
   )
 }
